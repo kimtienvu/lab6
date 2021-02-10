c36e3fb31d98eaf963b48d0e993a20f686fa2c22
kimtienvu <kvu@ucsd.edu>
2021-02-09 20:07:47 -0800

    added ID

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -8,7 +8,6 @@ function statusChangeCallback(response) {
   console.log('Facebook login status changed.');
   console.log(response);
   if (response.status === 'connected') {
-    // Logged into your app and Facebook.
         console.log('Successfully logged in with Facebook');
          FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
   }
